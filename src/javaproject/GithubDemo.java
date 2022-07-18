257e48ca2d06b7905091970033de22f03dcc4a46
punam <punamdeore2905@gmail.com>
2022-07-18 10:00:33 +0000

    First Project commited

---
+++ src/javaproject/GithubDemo.java
@@ -0,0 +1,11 @@
+package javaproject;
+
+public class GithubDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="message";
+System.out.println(str);
+	}
+
+}
